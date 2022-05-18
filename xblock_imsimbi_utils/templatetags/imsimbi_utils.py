e6271ea529e46bf64e187489aa99b19119223c51
zakum1 <james.nicolson.za@gmail.com>
2022-05-18 10:22:05 -0700

    Added debug information

--- xblock_imsimbi_utils/templatetags/imsimbi_utils.py
+++ xblock_imsimbi_utils/templatetags/imsimbi_utils.py
@@ -17,6 +17,7 @@ def imsimbi_registration_url():
     Django template tag that outputs the URL of the registration page
     {% imsimbi_registration_url %}
     """
+    print('imsimbi_registration_url: ' + settings.IMSIMBI_REGISTRATION_URL)
     return settings.IMSIMBI_REGISTRATION_URL
 
 @register.filter(name="imsimbi_registration_url_email")
@@ -25,7 +26,8 @@ def imsimbi_registration_url_email(email):
     Django template filter to retur a registration rul with an email query parameter.
     {{ email|imsimbi_registration_url_email }}
     """
-    return settings.IMSIMBI_REGISTRATION_URL+"?email="+email
+    print('imsimbi_registration_url: ' + settings.IMSIMBI_REGISTRATION_URL + "?email=" + email)
+    return settings.IMSIMBI_REGISTRATION_URL + "?email=" + email
 
 
 @register.simple_tag(name="imsimbi_profile_url")
@@ -34,6 +36,7 @@ def imsimbi_profile_url():
     Django template tag that outputs the URL of the registration page
     {% imsimbi_registration_url %}
     """
+    print('imsimbi_profile_url: ' + settings.IMSIMBI_PROFILE_URL)
     return settings.IMSIMBI_PROFILE_URL
 
 @register.filter(name="imsimbi_account_activation_url")
@@ -42,6 +45,8 @@ def imsimbi_account_activation_url(openedx_url):
     Django template filter to return an activation url that is handled by the Imsimbi app rather than the openedx LMS.
     {{ openedx_url|imsimbi_account_activation_url }}
     """
-    return settings.IMSIMBI_ACTIVATION_URL+"?activation_url="+quote(openedx_url)
+    url = settings.IMSIMBI_ACTIVATION_URL+"?activation_url="+quote(openedx_url)
+    print('imsimbi_account_activation_url: ' + url)
+    return url
 
 
