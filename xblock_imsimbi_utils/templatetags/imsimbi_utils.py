af49df06862717e61bc05f107a08c25b387ad6f6
zakum1 <james.nicolson.za@gmail.com>
2022-05-18 19:13:58 -0700

    Changed simple tags to filters

--- xblock_imsimbi_utils/templatetags/imsimbi_utils.py
+++ xblock_imsimbi_utils/templatetags/imsimbi_utils.py
@@ -11,8 +11,8 @@ from urllib.parse import quote
 register = template.Library()  # pylint: disable=invalid-name
 
 
-@register.simple_tag(name="imsimbi_registration_url")
-def imsimbi_registration_url():
+@register.filter(name="imsimbi_registration_url")
+def imsimbi_registration_url(email):
     """
     Django template tag that outputs the URL of the registration page
     {% imsimbi_registration_url %}
@@ -30,8 +30,8 @@ def imsimbi_registration_url_email(email):
     return settings.IMSIMBI_REGISTRATION_URL + "?email=" + email
 
 
-@register.simple_tag(name="imsimbi_profile_url")
-def imsimbi_profile_url():
+@register.filter(name="imsimbi_profile_url")
+def imsimbi_profile_url(email):
     """
     Django template tag that outputs the URL of the registration page
     {% imsimbi_registration_url %}
